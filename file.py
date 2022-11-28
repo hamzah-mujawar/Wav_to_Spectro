252d0a715ccae77b3ed8198ed1669f5d2bfaa355
jetthreat <hamzah1faisal@gmail.com>
2022-11-28 16:14:23 +0400

    Add files via upload

---
+++ file.py
@@ -0,0 +1,25 @@
+import matplotlib.pyplot as plt
+from scipy import signal
+import os
+from scipy.io import wavfile
+dataset_path = 'notes_v2'
+
+
+for i, (dirpath, dirnames, filenames) in enumerate(
+    os.walk(dataset_path)):  # dirpath is the current folder path, dirnames is the names of all the sub folders and file names are all the file names
+    # if we are at the dataset folder (not the subfolders for genres/ need to be in genre level)
+    if dirpath is not dataset_path:
+        # process the files for the specific genre
+        for f in filenames:
+            file_path = os.path.join(dirpath, f)  # load audio file
+            sample_rate, samples = wavfile.read(file_path)
+            frequencies, times, spectrogram = signal.spectrogram(samples, sample_rate)
+
+            plt.pcolormesh(times, frequencies, spectrogram)
+            plt.gca().set_axis_off()
+            plt.subplots_adjust(top=1, bottom=0, right=1, left=0,
+                                hspace=0, wspace=0)
+            plt.margins(0, 0)
+            plt.gca().xaxis.set_major_locator(plt.NullLocator())
+            plt.gca().yaxis.set_major_locator(plt.NullLocator())
+            plt.savefig(file_path + ".png", bbox_inches ='tight', pad_inches = 0)
